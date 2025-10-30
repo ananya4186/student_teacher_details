e7cadbe77ea6026622665d7660795d4906da2397
Ananya jantli <jantliananya@gmail.com>
2025-10-30 15:49:34 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,9 @@
+name = "Ananya"
+age = 20
+usn = "24BCA123"
+address = "Hubballi, Karnataka"
+print("----- Student Details -----")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"USN: {usn}")
+print(f"Address: {address}")
